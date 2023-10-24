5c682b36329069f88460e85af476c0df666b0050
Tomáš Bažant <tbazant@suse.cz>
2023-10-24 15:15:31 +0200

    added article file and topicmap

---
+++ topicmaps/alp-dolomite-interactive-deployment-using-agama.mm
@@ -0,0 +1,144 @@
+<?xml version="1.0"?>
+<map version="1.0.1">
+  <!--Vytvořeno Minderem 1.15.6-->
+  <node ID="id_35" TEXT="ALP Dolomite Deployment" FOLDED="false" COLOR="#62a0ea" POSITION="left">
+    <edge STYLE="bezier" COLOR="#62a0ea" WIDTH="4"/>
+    <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+    <node ID="id_36" TEXT="Interactive deployment using Agama" FOLDED="false" COLOR="#f37329" POSITION="left">
+      <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+      <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      <node ID="id_37" TEXT="Intro to ALP" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_38" TEXT="Intro to ALP deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_39" TEXT="Consideraions before deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_40" TEXT="(Optional) Preparing virtual machine" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_41" TEXT="Deployment procedure using  Agama graphical UI" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_42" TEXT="Considerations after deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+    </node>
+    <node ID="id_61" TEXT="Automated deployment using Agama" FOLDED="false" COLOR="#3689e6" POSITION="left">
+      <edge STYLE="bezier" COLOR="#3689e6" WIDTH="4"/>
+      <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      <node ID="id_62" TEXT="Intro to ALP" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_63" TEXT="Intro to ALP deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_64" TEXT="Consideraions before deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_65" TEXT="(Optional) Preparing virtual machine" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_66" TEXT="Intro to automated Agama deployment" FOLDED="false" COLOR="#3689e6" POSITION="left">
+        <edge STYLE="bezier" COLOR="#3689e6" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        <node ID="id_67" TEXT="Profile-based deployment" FOLDED="false" COLOR="#3689e6" POSITION="left">
+          <edge STYLE="bezier" COLOR="#3689e6" WIDTH="4"/>
+          <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        </node>
+        <node ID="id_68" TEXT="Script-based deployment" FOLDED="false" COLOR="#3689e6" POSITION="left">
+          <edge STYLE="bezier" COLOR="#3689e6" WIDTH="4"/>
+          <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        </node>
+      </node>
+      <node ID="id_69" TEXT="Considerations after deployment" FOLDED="false" COLOR="#f37329" POSITION="left">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+    </node>
+    <node ID="id_50" TEXT="Deployment using raw disk image" FOLDED="false" COLOR="#68b723" POSITION="right">
+      <edge STYLE="bezier" COLOR="#68b723" WIDTH="4"/>
+      <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      <node ID="id_51" TEXT="Intro to ALP" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_52" TEXT="Intro to ALP deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_53" TEXT="Consideraions before deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_54" TEXT="(Optional) Preparing virtual machine" FOLDED="false" COLOR="#68b723" POSITION="right">
+        <edge STYLE="bezier" COLOR="#68b723" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_55" TEXT="Intro to raw disk deployment" FOLDED="false" COLOR="#68b723" POSITION="right">
+        <edge STYLE="bezier" COLOR="#68b723" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_56" TEXT="Deployment with JeOS firstboot" FOLDED="false" COLOR="#68b723" POSITION="right">
+        <edge STYLE="bezier" COLOR="#68b723" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_57" TEXT="First boot configuration" FOLDED="false" COLOR="#68b723" POSITION="right">
+        <edge STYLE="bezier" COLOR="#68b723" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        <node ID="id_58" TEXT="Ignition" FOLDED="false" COLOR="#68b723" POSITION="right">
+          <edge STYLE="linear" COLOR="#68b723" WIDTH="4"/>
+          <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        </node>
+        <node ID="id_59" TEXT="Combustion" FOLDED="false" COLOR="#68b723" POSITION="right">
+          <edge STYLE="linear" COLOR="#68b723" WIDTH="4"/>
+          <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+        </node>
+      </node>
+      <node ID="id_60" TEXT="Considerations after deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+    </node>
+    <node ID="id_43" TEXT="Interactive deployment on zSeries using Agama" FOLDED="false" COLOR="#c6262e" POSITION="right">
+      <edge STYLE="bezier" COLOR="#c6262e" WIDTH="4"/>
+      <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      <node ID="id_44" TEXT="Intro to ALP" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_45" TEXT="Intro to ALP deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_46" TEXT="Consideraions before deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_47" TEXT="(Optional) Preparing virtual machine" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_48" TEXT="Deployment procedure using Agama UI" FOLDED="false" COLOR="#c6262e" POSITION="right">
+        <edge STYLE="bezier" COLOR="#c6262e" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+      <node ID="id_49" TEXT="Considerations after deployment" FOLDED="false" COLOR="#f37329" POSITION="right">
+        <edge STYLE="bezier" COLOR="#f37329" WIDTH="4"/>
+        <font NAME="Sans" SIZE="11" BOLD="false" ITALIC="false"/>
+      </node>
+    </node>
+  </node>
+</map>
---
+++ topicmaps/deploying-alp-dolomite-interactively-using-agama/deploying-alp-dolomite-interactively-using-agama.md
@@ -0,0 +1,70 @@
+# ALP Dolomite Deployment
+
+  - Interactive deployment using Agama
+
+    - Intro to ALP
+
+    - Intro to ALP deployment
+
+    - Consideraions before deployment
+
+    - (Optional) Preparing virtual machine
+
+    - Deployment procedure using  Agama graphical UI
+
+    - Considerations after deployment
+
+  - Automated deployment using Agama
+
+    - Intro to ALP
+
+    - Intro to ALP deployment
+
+    - Consideraions before deployment
+
+    - (Optional) Preparing virtual machine
+
+    - Intro to automated Agama deployment
+
+      - Profile-based deployment
+
+      - Script-based deployment
+
+    - Considerations after deployment
+
+  - Deployment using raw disk image
+
+    - Intro to ALP
+
+    - Intro to ALP deployment
+
+    - Consideraions before deployment
+
+    - (Optional) Preparing virtual machine
+
+    - Intro to raw disk deployment
+
+    - Deployment with JeOS firstboot
+
+    - First boot configuration
+
+      - Ignition
+
+      - Combustion
+
+    - Considerations after deployment
+
+  - Interactive deployment on zSeries using Agama
+
+    - Intro to ALP
+
+    - Intro to ALP deployment
+
+    - Consideraions before deployment
+
+    - (Optional) Preparing virtual machine
+
+    - Deployment procedure using Agama UI
+
+    - Considerations after deployment
+
